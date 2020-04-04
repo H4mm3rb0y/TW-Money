25c09a30094816020ef7cf22ea9390eeb61b6c2e
Tim <tim1230@hotmail.de>
2020-04-04 19:53:18 +0200

    init

---
+++ src/main/java/de/h4mm3r/twmoney/twmoney.java
@@ -0,0 +1,28 @@
+package de.h4mm3r.twmoney;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "twmoney", name = "TW Money", version = "0", useMetadata = true)
+public class twmoney
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
